1c5cf7ef48a8acb1ba15a4bddb87b80e28303ec0
Shadowiser <tankomamoudou24@gmail.com>
2026-05-21 21:02:40 +0000

    commentcement du dashboard

--- dashboard.php
+++ dashboard.php
@@ -23,16 +23,35 @@ if ($exists->num_rows == 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de bord de </title>
+    <link rel="stylesheet" href="./styles/dashboard.css">
 </head>
-<header>
-    <div class="logo"></div>
-    <nav>
-        <ul>
 
-        </ul>
-    </nav>
+<header>
+    <div class="header">
+        <div class="logo">
+            <img src="./src/images/logo.png" alt="" id="logo">
+        </div>
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li><a href="index.php" class="nav-link">Accueil</a></li>
+                <li><a href="posts.php" class="nav-link">Posts</a></li>
+                <li><button class="disconnect-btn">Se déconnecter</button></li>
+            </ul>
+        </nav>
+    </div>
 </header>
-<main></main>
+<main>
+    <?php
+    if (isset($_SESSION["nom"])) {
+        $nom = $_SESSION["nom"];
+    }
+    echo "<h1 class='hi-text'>Bonjour $nom</h1>"
+
+    ?>
+    <form class="form-container" method="post" name="post_form">
+
+    </form>
+</main>
 <footer></footer>
 
 <body>
---
+++ posts.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lotus - Posts</title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<body>
+    <?php require("./includes/header.php");
+    echo showHeader("./src/images/logo.png");
+    ?>
+</body>
+
+</html>
